547a6cf8c0f7d29e401a415602633ba01f610825
jayanth sagar <jayanth@vlabs.ac.in>
2014-05-05 09:54:17 +0000

    allexec.sh edited online with Bitbucket

--- src/lab/allexec.sh
+++ src/lab/allexec.sh
@@ -2,6 +2,10 @@ export OPENCV_INC=/usr/include/opencv
 export OPENCV_LIB=/usr/lib/
 export OPENCV_LIB_OPTS="-lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_flann"
 
+sudo apt-get update
+sudo apt-get install gcc
+sudo apt-get update
+
 gcc  -I$OPENCV_INC codes/latest_affine.cpp -L$OPENCV_LIB $OPENCV_LIB_OPTS -o execs/affine.out
 gcc  -I$OPENCV_INC codes/im_arith.cpp -L$OPENCV_LIB $OPENCV_LIB_OPTS -o execs/arith.out
 gcc  -I$OPENCV_INC codes/im_dist.cpp -L$OPENCV_LIB $OPENCV_LIB_OPTS -o execs/dist.out
